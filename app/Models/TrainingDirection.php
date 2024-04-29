5c914926057589eeb89287e39039735650cdffe9
RANDRAT <justlehamed@gmail.com>
2024-04-29 18:51:27 +0500

    brainfuck

---
+++ app/Models/TrainingDirection.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TrainingDirection extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'head_OPOP_id',
+        'institute_id'
+    ];
+
+    public function institut()
+    {
+        return $this->belongsTo(Institut::class);
+    }
+
+    public function groups()
+    {
+        return $this->hasMany(Group::class);
+    }
+
+    public function head()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
